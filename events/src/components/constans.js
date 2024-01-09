788071df3b3e35489c1c4d6748984c174fa61119
MishchenkoArtem <The.Nesh@yandex.ru>
2024-01-09 11:15:03 +0300

    Update constans.js

--- events/src/components/constans.js
+++ events/src/components/constans.js
@@ -1,2 +1,5 @@
 export const arrayCounter = document.querySelectorAll('.counter__span');
 export const arrayBox = document.querySelectorAll('.playing-field__box');
+export const popup = document.querySelector('.popup');
+export const popupHeading = document.querySelector('.popup__heading');
+export const popupButton = document.querySelector('.popup__button');
