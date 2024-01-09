30e866479d935ef37cde3a6c466d44c7a5a45eec
MishchenkoArtem <The.Nesh@yandex.ru>
2024-01-09 11:15:05 +0300

    Update counter.js

--- events/src/components/counter.js
+++ events/src/components/counter.js
@@ -1,5 +1,8 @@
 import { arrayCounter } from './constans';
 import { arrayBox } from './constans';
+import { popup } from './constans';
+import { popupHeading } from './constans';
+import { popupButton } from './constans';
 
 export function eventListener() {
     arrayBox.forEach(item => {
@@ -10,15 +13,23 @@ export function eventListener() {
                 arrayCounter[0].textContent++;
 
                 if (arrayCounter[0].textContent === '5') {
-                    alert('victory');
+                    popup.classList.add('popup_opened');
+                    popupHeading.textContent = 'Победа';
                 }
             } else {
                 arrayCounter[1].textContent++;
 
                 if (arrayCounter[1].textContent === '5') {
-                    alert('defeat');
+                    popup.classList.add('popup_opened');
+                    popupHeading.textContent = 'Поражение';
                 }
             }
         });
     });
 }
+
+popupButton.addEventListener('click', () => {
+    popup.classList.remove('popup_opened');
+    arrayCounter[0].textContent = 0;
+    arrayCounter[1].textContent = 0;
+});
